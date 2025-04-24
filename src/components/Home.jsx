039eecc9d0784449ca9e19093572446a3593daf9
Ryann <ryann.hall2004@gmail.com>
2025-04-23 22:32:20 -0400

    some styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,11 @@
 function home(){
     return(
         <div className="home">
+            <h1>30% OFF</h1>
+            <p>Site-Wide</p>
+            <button>use code: GAG30</button>
+            {/* <h1>30% OFF</h1>
+            <h2>Site-Wide</h2> */}
         </div>
     )
 }
